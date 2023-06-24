6a7144d86f9811f6f0174822872e33eb1b58e0d8
Daiki925864 <daikiokada9097@gmail.com>
2023-06-24 20:45:30 +0900

    投稿機能を実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,58 @@
 class PostsController < ApplicationController
-  def index
-    
-  end
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_post, only: [:edit, :show, :update, :destroy]
+  before_action :move_to_index, except: [:index, :show, :new, :create]
   
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    @post.update(post_params)
+    if @post.save
+      redirect_to prototype_path(@post.id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content, :shooting_date, :image).merge(user_id: current_user.id)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def move_to_index
+    unless user_signed_in? && current_user == @post.user
+      redirect_to action: :index
+    end
+  end
 end
