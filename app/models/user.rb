bfd5e5f76a52beb2577e5731a6b7a08e87510b43
Daiki925864 <daikiokada9097@gmail.com>
2023-06-24 14:27:53 +0900

    バリデーションを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates_length_of :password, minimum: 6
 end
