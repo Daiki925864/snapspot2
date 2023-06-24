97119e0748e9bfebb481e1d0831df25043c4018b
Daiki925864 <daikiokada9097@gmail.com>
2023-06-24 19:55:44 +0900

    バリデーションを設定

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,12 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  with_options presence: true do
+  validates :image
+  validates :title
+  validates :content
+  validates :shooting_date
+  end
+
 end
