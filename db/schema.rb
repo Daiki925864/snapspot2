ac4e9c2178f1090322a19e7f9c7c040248c63b3e
Daiki925864 <daikiokada9097@gmail.com>
2023-06-25 23:06:01 +0900

    緯度経度・住所カラムの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_24_105023) do
+ActiveRecord::Schema.define(version: 2023_06_25_135211) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -40,6 +40,9 @@ ActiveRecord::Schema.define(version: 2023_06_24_105023) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "address"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
