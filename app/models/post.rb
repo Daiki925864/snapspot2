f7f8cbd75499cc6d5aa1eff5b2bc8aaaf3491a54
Daiki925864 <daikiokada9097@gmail.com>
2023-07-02 17:26:16 +0900

    アソシエーションを設定

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
+  has_many :post_tag_relations
+  has_many :tags, through: :post_tag_relations
   has_one_attached :image
 
   geocoded_by :address
