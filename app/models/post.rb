7a043986b6cab9edc7f719015bd9972dcaf7511c
Daiki925864 <daikiokada9097@gmail.com>
2023-07-06 00:16:20 +0900

    destroyアクションの修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
-  has_many :post_tag_relations
-  has_many :tags, through: :post_tag_relations
+  has_many :post_tag_relations, dependent: :destroy
+  has_many :tags, through: :post_tag_relations, dependent: :destroy
   has_one_attached :image
 
   geocoded_by :address
