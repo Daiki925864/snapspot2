cf1463c6b37736c19fabce55405f0d3a2682b01a
Daiki925864 <daikiokada9097@gmail.com>
2023-07-06 00:26:20 +0900

    モデルの修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
   has_many :post_tag_relations, dependent: :destroy
-  has_many :tags, through: :post_tag_relations, dependent: :destroy
+  has_many :tags, through: :post_tag_relations
   has_one_attached :image
 
   geocoded_by :address
